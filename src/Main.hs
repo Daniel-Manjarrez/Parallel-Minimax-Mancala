e900f379a745eb399e782a7a05f18e576ce08e41
CaiwuChen <96741036+CaiwuChen@users.noreply.github.com>
2024-12-18 23:27:52 -0500

    Update Main.hs

--- src/Main.hs
+++ src/Main.hs
@@ -6,18 +6,13 @@ import GameState (GameState(..), Player(..))
  
 main :: IO ()
 main = do
-  -- Get command-line arguments
   args <- getArgs
-  
-  -- Check if a depth argument is provided
+ 
   case args of
     [depthStr, parallelDepthStr] -> do
-      -- Convert the input string to an integer
       let depth = read depthStr :: Int
           parallelDepth = read parallelDepthStr :: Int
-      -- Print a starting message
       putStrLn "Starting Mancala Game"
-      -- Start the game with the initial state and the given depth
       let initialState = GameState [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0] Player1
       playGame initialState depth parallelDepth
     _ -> do
